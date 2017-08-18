11093c03d067d46c73cd6705aa59866170d0cdf3
Mark Lockett <mlockett@bigpond.com>
2017-08-19 07:21:05 +0800

    Clean up demo and test the python code executes

--- cav_demo.py
+++ cav_demo.py
@@ -1,3 +1,12 @@
 from __future__ import absolute_import, print_function
 import js
 
+jquery = js.globals["$"]
+
+preview_div = jquery(".previewdiv")[0]
+
+preview_div.innerHTML = "<h2>heading 2</h2>"
+
+
+print ("Hello cav demo")
+
