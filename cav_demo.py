341669ebd4989448fdccbf0d7a311f4fcfbd457f
Mark Lockett <mlockett@bigpond.com>
2017-08-19 10:11:46 +0800

    Get a decent demo with cavorite in a module

--- cav_demo.py
+++ cav_demo.py
@@ -1,12 +1,16 @@
 from __future__ import absolute_import, print_function
 import js
+from cavorite import c
 
 jquery = js.globals["$"]
 
 preview_div = jquery(".previewdiv")[0]
 
-preview_div.innerHTML = "<h2>heading 2</h2>"
+content = c("div", [c("h1", "Cavorite Test"), c("p", "It works"), c("a", {"href": "https://www.google.com"}, "Google")])
+
+content.render(preview_div)
 
 
-print ("Hello cav demo")
 
+        
+            
