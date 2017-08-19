0f1e28ccebd576d9689b20bb39f010b78a767eaa
Mark Lockett <mlockett@bigpond.com>
2017-08-19 12:45:20 +0800

    Demonstrate calling back from javascript

--- cav_demo.py
+++ cav_demo.py
@@ -6,7 +6,18 @@ jquery = js.globals["$"]
 
 preview_div = jquery(".previewdiv")[0]
 
-content = c("div", [c("h1", "Cavorite Test"), c("p", "It works"), c("a", {"href": "https://www.google.com"}, "Google")])
+class Printer(object):
+    def __init__(self, message):
+        self.message = message
+    def print_it(self, e):
+        print(self.message)
+
+p = Printer("Hello me!!!")
+
+content = c("div", [c("h1", "Cavorite Test"),
+                    c("p", "It works"),
+                    c("a", {"href": "https://www.google.com"}, "Google"),
+                    c("button", {"onclick": p.print_it}, "Press me")])
 
 content.render(preview_div)
 
