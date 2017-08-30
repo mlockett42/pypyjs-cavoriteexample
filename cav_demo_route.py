fb074bfdc0b42de4085f4ee974082d2e2f7f83e1
Mark Lockett <mlockett@bigpond.com>
2017-08-30 22:01:05 +0800

    Add links to github for this project

--- cav_demo_route.py
+++ cav_demo_route.py
@@ -41,7 +41,10 @@ welcome_page = c("div", [c("p", "Welcome to cavorite"),
                          c("p", [c("a", {"href": "/#!levels/110"}, "Level 110")]),
                          c("p", [c("a", {"href": "/#!levels/119"}, "Level 119")]),
                          c("p", [c("a", {"href": "/cav_demo_mount.html"}, "Mount Demo")]),
-                         c("p", [c("a", {"href": "/cav_demo_render.html"}, "Render Demo")])])
+                         c("p", [c("a", {"href": "/cav_demo_render.html"}, "Render Demo")]),
+                         c("p", [c("a", {"href": "https://github.com/mlockett42/cavorite"}, "Cavorite - Github")]),
+                         c("p", [c("a", {"href": "https://github.com/mlockett42/pypyjs-cavoriteexample"}, "pypyjs-cavoriteexample - Github")])
+                         ])
 
 error_404_page = c("div", [c("p", "No match 404 error"),
                            c("p", [c("a", {"href": "/#!"}, "Back to main page")])])
